5ea32e2edb0d432154cab33613d0c74db80aa233
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-06-25 00:44:01 -0400

    functional

--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,53 @@
 $(document).ready(function () {
-
-    var newAnimal = $("#search-input").val().trim();
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        newAnimal + "&api_key=dc6zaTOxFJmzC&limit=6";
     
+    var newAnimal
+    var animalList = ["dog"];
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var results = response.data;
 
-        for (var i = 0; i < results.length; i++) {
-            var newGif = $("<div class='gifs'>");
-            var imgTag = $("<img>");
-            imgTag.attr("src", results[i].images.fixed_height.url);
 
-            $("#gifs-4ever").prepend(newGif);
-        } // end for loop
+    function newButtons() {
+        $("#new-buttons").empty();
+        for (var i = 0; i < animalList.length; i++) {
+            var btnTag = $("<button>")
+            btnTag.addClass("animalBtn");
+            btnTag.attr("data-name", animalList[i]);
+            btnTag.text(animalList[i]);
+            $("#new-buttons").append(btnTag);
+        }
+    }
+
+    $("#add-animal").on("click", function (event) {
+        event.preventDefault();
+        newAnimal = $("#search-input").val().trim();
+        animalList.push(newAnimal);
+        newButtons();
+        /*Test*/console.log(animalList);
+    });
+
+    $(document).on("click", ".animalBtn", function () {
+        var thisAnimal = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        thisAnimal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var results = response.data;
+
+            for (var j = 0; j < results.length; j++) {
+                var newGif = $("<div class='gifs'>");
+                var imgTag = $("<img>");
+                imgTag.attr("src", results[j].images.fixed_height.url);
+
+                newGif.prepend(imgTag);
+
+                $("#gifs-4ever").prepend(newGif);
+                /*Test*/console.log(queryURL)
+            } // end for loop
+        });
+
     });
 
 
